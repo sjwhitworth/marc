520cbf587cb713b397b920b206bb7fe6ce379571
Stephen Whitworth <stephen.whitworth@hailocab.com>
2015-03-11 09:17:45 +0000

    Cleaning up, CLI-ing

---
+++ README.md
@@ -0,0 +1,7 @@
+##Marc
+
+The markov-chain-junk-text-generator. Feed it a filepath to a text file, and it'll generate pseudo-real text, based upon the corpus that it was provided. Good fun.
+
+```
+"Jonathan Ive has reluctantly accepted the services of a chimpanzee, and a half billion things designed by Ive."
+```
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
 func main() {
+	fpath := flag.String("f", "", "The filepath of the text file")
+	length := flag.Int("length", 10, "The length of sentence in words")
+	flag.Parse()
+
+	if *fpath == "" {
+		fmt.Println("[Error] I need a valid filepath in order to be able to spew junk.")
+		return
+	}
+
 	m := NewMarkov()
-	err := m.Seed("/Users/stephenwhitworth/Desktop/test.txt")
+	err := m.Seed(*fpath)
 	if err != nil {
-		panic(err)
+		fmt.Println("[Error] Could not open file, check your filepath is all good.")
 	}
 
 	for {
-		fmt.Println(m.GenerateText(100))
-		time.Sleep(1 * time.Second)
+		fmt.Println(m.GenerateText(*length))
+		time.Sleep(5 * time.Second)
 	}
 }
